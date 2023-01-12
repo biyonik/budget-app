043f4c4e4491bbb35ba819cb6e5ca0a85009b1cf
ahmet <ahmet.altun60@gmail.com>
2023-01-12 21:59:45 +0300

    TransactionHeader componenti tasarlandı

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,19 @@
 <script>
     import Header from "./lib/Layout/Header/Header.svelte";
     import Sidebar from "./lib/Layout/Sidebar/Sidebar.svelte";
+    import TransactionHeader from "./lib/Layout/Content/TransactionHeader/TransactionHeader.svelte";
 </script>
 
 <Header/>
 <main class="p-1">
     <div class="d-flex flex-column flex-sm-row">
-        <Sidebar />
-        <div class="flex-grow-1 pt-3" aria-describedby="Sidebar">
+        <Sidebar/>
+        <div class="flex-grow-1 pt-3" aria-describedby="Content">
+            <TransactionHeader/>
             <div aria-describedby="Transaction Card List">
-
             </div>
 
             <div aria-describedby="Transaction Table View">
-
             </div>
         </div>
     </div>
---
+++ src/lib/Layout/Content/TransactionHeader/TransactionHeader.svelte
@@ -0,0 +1,60 @@
+<div class="d-flex flex-column flex-sm-row justify-content-around">
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+        <div class="card-text fs-3 fw-bolder dark-indigo">Income: <span class="text-danger">1</span></div>
+        <div class="card-text fs-3 fw-bolder dark-indigo">Transactions: <span class="text-danger">11</span></div>
+        <div class="card-text fs-3 fw-bolder dark-indigo">Budget: <span class="text-danger">111</span></div>
+    </div>
+
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+        <div class="card-text fs-2 fw-bolder dark-indigo">
+            Investments
+        </div>
+        <div class="d-flex flex-column ms-1">
+            <div class="d-flex align-items-center">
+                <div class="bg-primary rounded-circle w-10px h-10px"></div>
+                <div class="ms-2 card-text">5 transactions</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <div class="bg-warning rounded-circle w-10px h-10px"></div>
+                <div class="ms-2 card-text">$3000</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+        <div class="card-text fs-2 fw-bolder dark-indigo">
+            Expenses
+        </div>
+        <div class="d-flex flex-column ms-1">
+            <div class="d-flex align-items-center">
+                <div class="bg-primary rounded-circle w-10px h-10px"></div>
+                <div class="ms-2 card-text">5 transactions</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <div class="bg-warning rounded-circle w-10px h-10px"></div>
+                <div class="ms-2 card-text">$3000</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .t-card {
+        background-color: #C5CBD8;
+        max-width: 75%;
+    }
+
+    .dark-indigo {
+        color: #3B5998;
+    }
+
+    .w-10px {
+        width: 10px;
+    }
+
+    .h-10px {
+        height: 10px;
+    }
+</style>
