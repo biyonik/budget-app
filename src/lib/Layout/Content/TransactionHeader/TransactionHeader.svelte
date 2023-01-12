ed7ceef25b616b04d2a76f6a7bc094729a4783e8
ahmet <ahmet.altun60@gmail.com>
2023-01-12 23:04:15 +0300

    Sveltestrap eklendi, scss desteği eklendi, transactionHeader componenti tamamlandı

--- src/lib/Layout/Content/TransactionHeader/TransactionHeader.svelte
+++ src/lib/Layout/Content/TransactionHeader/TransactionHeader.svelte
@@ -1,11 +1,15 @@
+<script>
+    import { Progress } from 'sveltestrap';
+</script>
+
 <div class="d-flex flex-column flex-sm-row justify-content-around">
-    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded" aria-describedby="First Card">
         <div class="card-text fs-3 fw-bolder dark-indigo">Income: <span class="text-danger">1</span></div>
         <div class="card-text fs-3 fw-bolder dark-indigo">Transactions: <span class="text-danger">11</span></div>
         <div class="card-text fs-3 fw-bolder dark-indigo">Budget: <span class="text-danger">111</span></div>
     </div>
 
-    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded" aria-describedby="Second Card">
         <div class="card-text fs-2 fw-bolder dark-indigo">
             Investments
         </div>
@@ -20,9 +24,14 @@
                 <div class="ms-2 card-text">$3000</div>
             </div>
         </div>
+        <Progress value={35} color="danger" class="mt-1 mb-1"/>
+        <span class="font-size-12">
+            Investments / Incomes:
+            <span class="fw-bolder">{75}%</span>
+        </span>
     </div>
 
-    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded">
+    <div class="t-card d-flex flex-column shadow p-3 mb-5 rounded" aria-describedby="Thirth Card">
         <div class="card-text fs-2 fw-bolder dark-indigo">
             Expenses
         </div>
@@ -37,13 +46,31 @@
                 <div class="ms-2 card-text">$3000</div>
             </div>
         </div>
+        <Progress value={35} color="danger" class="mt-1 mb-1"/>
+        <span class="font-size-12">
+            Expenses / Incomes:
+            <span class="fw-bolder">{35}%</span>
+        </span>
     </div>
 </div>
 
-<style>
+<style type="text/scss">
     .t-card {
         background-color: #C5CBD8;
         max-width: 75%;
+        min-width: 25%;
+    }
+
+    @media(max-width:768px) {
+      .t-card {
+        min-width: 75%;
+      }
+    }
+
+    @media(max-width:801px) {
+      .card-text {
+        font-size: 15px;
+      }
     }
 
     .dark-indigo {
@@ -56,5 +83,9 @@
 
     .h-10px {
         height: 10px;
+    }
+
+    .font-size-12 {
+        font-size: 12px;
     }
 </style>
